b8c47ea544b359c7c81cfb278e8910f1d1e07987
TatianaKan <klanukt@gmail.com>
2022-03-20 19:30:51 +0300

    tabs через функцию

--- portfolio-main/js/tabs.js
+++ portfolio-main/js/tabs.js
@@ -3,30 +3,26 @@ const tabDescription = document.querySelectorAll('.design__descr');
 
 const tabImages = document.querySelectorAll('.design-images')
 
-console.log(tabDescription);
-
-tabsBtns.forEach((tabsBtn, index) => {
-  tabsBtn.addEventListener('click', (event) => {
-
-   tabDescription.forEach((descr, indexDescr) => {
-     if (index === indexDescr) {
-       descr.classList.remove('hidden');
-     } else {
-      descr.classList.add('hidden');
-     }
-   })
-
-   tabImages.forEach((image, indeximage) => {
-    if (index === indeximage) {
-      image.classList.remove('hidden');
+const changeContent = (array, value) => {
+  array.forEach((elem) => {
+    if (elem.dataset.tabsField === value) {
+      elem.classList.remove('hidden');
     } else {
-     image.classList.add('hidden');
+      elem.classList.add('hidden');
     }
   })
+};
 
+tabsBtns.forEach((tabsBtn) => {
+  tabsBtn.addEventListener('click', (event) => {
+
+    const dataValue = tabsBtn.dataset.tabsHandler;
+
+    changeContent(tabDescription, dataValue);
+    changeContent(tabImages, dataValue);
 
     tabsBtns.forEach((btn) => {
-     
+
       if (btn === event.target) {
         btn.classList.add('design-list__item_active');
       } else {
