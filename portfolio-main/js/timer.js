bef549a4280ff8e67a15ec4b52aba64ef4589cc8
TatianaKan <klanukt@gmail.com>
2022-03-20 22:08:08 +0300

    обратный отсчет

--- portfolio-main/js/timer.js
+++ portfolio-main/js/timer.js
@@ -1,19 +1,33 @@
 const timerBlock = document.querySelector('.timer__time');
+const deadline = '31 march 2022';
+
+let interval;
 
 const updateClock = () => {
-  const date = new Date();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
+  const date = new Date().getTime();
+  const dateDeadline = new Date(deadline).getTime();
+  const timeRemaining = (dateDeadline - date) / 1000;
+
+  const hours = Math.floor(timeRemaining / 60 / 60);
+  const minutes = Math.floor((timeRemaining / 60) % 60);
+  const seconds = Math.floor(timeRemaining % 60);
 
   const fhours = hours < 10 ? '0' + hours : hours;
   const fminutes = minutes < 10 ? '0' + minutes : minutes;
   const fseconds = seconds < 10 ? '0' + seconds : seconds;
 
   timerBlock.textContent = `${fhours}:${fminutes}:${fseconds}`;
+
+  if (timeRemaining <= 0) {
+    clearInterval(interval);
+
+    timerBlock.textContent = `00:00:00`;
+  }
 };
 
 updateClock();
 
-setInterval(updateClock, 500);
+interval = setInterval(updateClock, 500);
+
+
 
